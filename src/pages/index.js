db3bcd28cf081d0e0577dca67ec732257abcaeb9
subinsong01 <haely97@naver.com>
2025-07-03 22:57:32 +0900

    feat: Footer 적용

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <div className="max-w-[940px] mx-auto">
       <Header />
+      <Footer />
     </div>
   );
 }
