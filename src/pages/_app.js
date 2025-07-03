270e7a89cb453359dfec19ebeb787c0a8b61e513
subinsong01 <haely97@naver.com>
2025-07-04 00:18:39 +0900

    feat: 다크모드 적용

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,18 @@
+import useDarkMode from "@/hook/useDarkMode";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const [isDark, toggleDarkMode] = useDarkMode();
+
+  return (
+    <>
+      <main className="max-w-[940px] mx-auto min-h-screen bg-white text-black dark:bg-black dark:text-white">
+        <Header isDark={isDark} toggleDarkMode={toggleDarkMode} />
+        <Component {...pageProps} />
+        <Footer />
+      </main>
+    </>
+  );
 }
