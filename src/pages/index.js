9cc446fd9e87226306873953a0017622b2ea51bc
subinsong01 <haely97@naver.com>
2025-07-04 00:19:52 +0900

    chore: tailwind v3으로 재설치

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,3 @@
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
 export default function Home() {
-  return (
-    <div className="max-w-[940px] mx-auto">
-      <Header />
-      <Footer />
-    </div>
-  );
+  return <div className="max-w-[940px] mx-auto"></div>;
 }
