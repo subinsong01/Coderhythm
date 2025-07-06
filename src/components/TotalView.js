1050917217ac596ad894caa46bd3393067761c5c
subinsong01 <haely97@naver.com>
2025-07-06 18:30:41 +0900

    feat: Firebase 연동한 전체 조회수 컴포넌트 구현

---
+++ src/components/TotalView.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+import { getTotalViews } from "../../lib/incrementView";
+
+export default function TotalView() {
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    getTotalViews()
+      .then(setTotal)
+      .catch(() => setTotal(0));
+  }, []);
+
+  return (
+    <div className="text-right sm:text-sm text-xs text-gray-600 dark:text-gray-400">
+      조회수 | <span className="font-semibold">{total.toLocaleString()}회</span>
+    </div>
+  );
+}
