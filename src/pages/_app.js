e1f6f84a8b4ddf9d2bc398ea6b9977f50a22000a
subinsong01 <haely97@naver.com>
2025-07-06 18:29:40 +0900

    feat: 방문자 수 컴포넌트 삽입

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,15 +2,16 @@ import useDarkMode from "@/hook/useDarkMode";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import "@/styles/globals.css";
+import TotalView from "@/components/TotalView";
 
 export default function App({ Component, pageProps }) {
   const [isDark, toggleDarkMode] = useDarkMode();
 
   return (
-    <div className="min-h-screen bg-white text-black dark:bg-black dark:text-white">
+    <div className="min-h-screen bg-white text-black dark:bg-black dark:text-white px-4">
       <div className="max-w-[940px] mx-auto  flex flex-col min-h-screen">
         <Header isDark={isDark} toggleDarkMode={toggleDarkMode} />
-
+        <TotalView />
         <main className="flex-grow">
           <Component {...pageProps} />
         </main>
