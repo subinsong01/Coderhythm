700d73a4480526bd983f1eedc56377aabdf6e577
subinsong01 <haely97@naver.com>
2025-07-06 18:30:01 +0900

    feat: 메인페이지에 방문자 수 기능 붙임

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
+import { useEffect } from "react";
+import { incrementViewCount } from "../../lib/incrementView";
 export default function Home() {
+  useEffect(() => {
+    incrementViewCount("home");
+  }, []);
+
   return <div className="max-w-[940px] mx-auto"></div>;
 }
