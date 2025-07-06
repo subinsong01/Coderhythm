da46ab7d1f1f6f8bd5b9ca547cacb2649ced874d
subinsong01 <haely97@naver.com>
2025-07-06 18:29:17 +0900

    style: padding 값 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 export default function Header({ isDark, toggleDarkMode }) {
   return (
-    <header className="py-4 px-6 flex justify-between">
+    <header className="py-4 flex justify-between">
       <h1 className="text-sm sm:text-xl font-bold text-gray-800 dark:text-white">
         모던 리액트 Deep Dive
       </h1>
