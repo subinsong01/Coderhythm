e0bb9c727406b56bd5056bce29e250995508ad30
subinsong01 <haely97@naver.com>
2025-07-14 00:05:36 +0900

    feat: 띄어쓰기 적용

--- src/components/TotalView.js
+++ src/components/TotalView.js
@@ -11,7 +11,7 @@ export default function TotalView() {
   }, []);
 
   return (
-    <div className="text-right sm:text-sm text-xs text-gray-600 dark:text-gray-400">
+    <div className="text-right sm:text-sm text-xs text-gray-600 dark:text-gray-400 px-4 ml-auto">
       조회수 | <span className="font-semibold">{total.toLocaleString()}회</span>
     </div>
   );
