650cb0b4d730660ed865a76badd4144da2b39cdf
subinsong01 <haely97@naver.com>
2025-07-15 15:27:35 +0900

    feat: getStaticProps에 next-i18next 설정 추가하여 다국어 번역 지원 적용

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,17 @@
 import { useEffect } from "react";
 import { incrementViewCount } from "../../lib/incrementView";
 import Comments from "@/components/Comments";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import nextI18NextConfig from "../../next-i18next.config";
+
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"], nextI18NextConfig)),
+    },
+  };
+}
+
 export default function Home() {
   useEffect(() => {
     incrementViewCount("home");
